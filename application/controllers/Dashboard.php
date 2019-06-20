0b6643f8f836438bf78c0e15e9f17f44ab40e961
thaksinai-git <52022506+thaksinai-git@users.noreply.github.com>
2019-06-20 12:23:14 +0700

    application

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function index()
+	{
+		echo "Dashboard";
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/Dashboard.php */
---
+++ application/modules/fn/controllers/Account_controller.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account_controller extends CI_Controller {
+
+	public function index()
+	{
+		echo "Account_controller";
+	}
+
+}
+
+/* End of file account_controller.php */
+/* Location: ./application/controllers/account_controller.php */
